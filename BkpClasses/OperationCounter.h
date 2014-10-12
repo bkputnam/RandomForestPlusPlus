5c6f3265f881fa66c8bddcd6c0cce02a2832d10c
Brian Putnam <brian@brian-putnam.com>
2014-10-12 13:32:48 -0600

    moved fn definition from .h to .cpp

--- BkpClasses/OperationCounter.h
+++ BkpClasses/OperationCounter.h
@@ -37,31 +37,7 @@ namespace bkp {
         OperationCounter& operator=(const OperationCounter& copyFrom);
         OperationCounter& operator=(OperationCounter&& moveFrom);
         
-        static void PrintAll(bool ignoreZeros=true) {
-            using std::cout;
-            using std::endl;
-            
-            cout << std::left;
-            #define CONDITIONAL_PRINT(name) {\
-                if (!(ignoreZeros && name==0)) {\
-                    cout << std::setw(20) << #name ":" << name << endl;\
-                }\
-            }
-            
-            CONDITIONAL_PRINT(default_constructors)
-            CONDITIONAL_PRINT(int_constructors)
-            CONDITIONAL_PRINT(instance_count)
-            CONDITIONAL_PRINT(destructors)
-            CONDITIONAL_PRINT(copy_constructors)
-            CONDITIONAL_PRINT(move_constructors)
-            CONDITIONAL_PRINT(copy_assignments)
-            CONDITIONAL_PRINT(move_assignments)
-            
-            // cleanup
-            #undef CONDITIONAL_PRINT
-            cout << std::right; // don't know if the std::left bit lives on after this function; undo it just in case.
-            // note: std::setw seems to only live as long as the next << action, don't need to undo it.
-        }
+        static void PrintAll(bool ignoreZeros=true);
     };
 
     void ResetOperationsCounter();
