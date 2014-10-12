787c14b2a2a7af1e8c3c4b22f623d6abcd9cb47c
Brian Putnam <brian@brian-putnam.com>
2014-10-12 13:23:53 -0600

    Added utility fn to print OperationCounter stats

--- BkpClasses/OperationCounter.h
+++ BkpClasses/OperationCounter.h
@@ -9,6 +9,9 @@
 #ifndef __RandomForest____OperationCounter__
 #define __RandomForest____OperationCounter__
 
+#include <iostream>
+#include <iomanip>
+
 namespace bkp {
     
     class OperationCounter {
@@ -33,6 +36,32 @@ namespace bkp {
         
         OperationCounter& operator=(const OperationCounter& copyFrom);
         OperationCounter& operator=(OperationCounter&& moveFrom);
+        
+        static void PrintAll(bool ignoreZeros=true) {
+            using std::cout;
+            using std::endl;
+            
+            cout << std::left;
+            #define CONDITIONAL_PRINT(name) {\
+                if (!(ignoreZeros && name==0)) {\
+                    cout << std::setw(20) << #name ":" << name << endl;\
+                }\
+            }
+            
+            CONDITIONAL_PRINT(default_constructors)
+            CONDITIONAL_PRINT(int_constructors)
+            CONDITIONAL_PRINT(instance_count)
+            CONDITIONAL_PRINT(destructors)
+            CONDITIONAL_PRINT(copy_constructors)
+            CONDITIONAL_PRINT(move_constructors)
+            CONDITIONAL_PRINT(copy_assignments)
+            CONDITIONAL_PRINT(move_assignments)
+            
+            // cleanup
+            #undef CONDITIONAL_PRINT
+            cout << std::right; // don't know if the std::left bit lives on after this function; undo it just in case.
+            // note: std::setw seems to only live as long as the next << action, don't need to undo it.
+        }
     };
 
     void ResetOperationsCounter();
