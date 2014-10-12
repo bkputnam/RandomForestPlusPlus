11353d9b633336b15b880a1d80ba029b16eaa097
Brian Putnam <brian@brian-putnam.com>
2014-10-12 14:33:10 -0600

    not sure if important: TContainer is now const&

--- BkpClasses/Arr.h
+++ BkpClasses/Arr.h
@@ -55,7 +55,7 @@ namespace bkp {
         // iterator constructor
         // Note: requires size() function on TContainer to work properly
         template<typename TContainer>
-        explicit Arr(TContainer srcCtnr) : Arr(srcCtnr.size(), srcCtnr.begin(), srcCtnr.end()) { }
+        explicit Arr(const TContainer& srcCtnr) : Arr(srcCtnr.size(), srcCtnr.begin(), srcCtnr.end()) { }
         
         // destructor
         virtual ~Arr() {
