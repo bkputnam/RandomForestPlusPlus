3ee47fc0ca5f2e6e3a8703aa8056f0ed68cce4a4
Brian Putnam <brian@brian-putnam.com>
2014-10-17 17:24:21 -0600

    commenting out RowSet

--- RandomForest++/RowSet.cpp
+++ RandomForest++/RowSet.cpp
@@ -1,9 +1,9 @@
+////
+////  RowSet.cpp
+////  RandomForest++
+////
+////  Created by Brian Putnam on 10/6/14.
+////  Copyright (c) 2014 Brian Putnam. All rights reserved.
+////
 //
-//  RowSet.cpp
-//  RandomForest++
-//
-//  Created by Brian Putnam on 10/6/14.
-//  Copyright (c) 2014 Brian Putnam. All rights reserved.
-//
-
-#include "RowSet.h"
+//#include "RowSet.h"
