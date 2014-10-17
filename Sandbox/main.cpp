36d15fba87b90342c086dfb6217b41eaaa88c8f4
Brian Putnam <brian@brian-putnam.com>
2014-10-16 19:49:38 -0600

    testing out thread stuff

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -7,30 +7,50 @@
 //
 
 #include <iostream>
-#include <iomanip>
-#include <memory>
-#include <string>
-#include "OperationCounter.h"
-#include "Arr.h"
-#include "Range.h"
-#include "Slice.h"
+#include <thread>
+#include <mutex>
+#include <boost/thread.hpp>
+
+#include "XRange.h"
 
 using std::cout;
 using std::cin;
 using std::endl;
 
-class SomeClass {
-public:
-    std::shared_ptr<int> sptr;
-    SomeClass() { }
-};
-
 using namespace bkp;
 using namespace std;
 
+std::mutex cout_mutex;
+
+boost::thread_specific_ptr<int> threadsum;
+
+void sum(int i) {
+    if (!threadsum.get()) {
+        threadsum.reset(new int);
+    }
+    *threadsum = 0;
+    int ctr = i;
+    while (ctr) {
+        *threadsum += ctr;
+        --ctr;
+    }
+    {
+        std::lock_guard<std::mutex> get_mutex(cout_mutex);
+        cout << "sum(" << i << ") = " << *threadsum << endl;
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    Range r(0, 10);
-    RangeIter iter = r.begin();
-    RangeIter end = r.end();
-    ++iter;
+    
+    const int NUM_THREADS = 10;
+    
+    std::vector<std::thread> threads;
+    threads.reserve(NUM_THREADS);
+    for (int i=0; i<NUM_THREADS; i++) {
+        threads[i] = std::thread(sum, i*10);
+    }
+    
+    for (int i=0; i<NUM_THREADS; i++) {
+        threads[i].join();
+    }
 }
